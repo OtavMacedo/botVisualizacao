1cb9366ad9a70a67f24c7ac1e3f9f6514cf90e44
Otavio Augusto <151087988+OtavMacedo@users.noreply.github.com>
2024-03-21 14:36:32 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import requests
 import time
 
+#url de exemplo
 url = "https://camo.githubusercontent.com/1817b38e81e8ac757188d0c9fc095551af4c33276905ff3aa931eb85a61aec2c/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f7b477265636f50657265737d2f636f756e742e737667"
 num_atualizacoes = 10000
 
